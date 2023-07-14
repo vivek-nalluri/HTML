278946ed29820026ec26d99575b8a9511a3b9dea
vivek-nalluri <vivek94947@gmail.com>
2023-07-14 19:13:41 +0530

    ok

---
+++ usha-rama.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+    let loader = document.querySelector(".load");
+    loader.style.display="none";
+    let load = document.querySelector(".bc");
+    loader.style.display = "none";
+},3500)
